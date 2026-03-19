661dae2ea55642e9cb7c5b300b2d62658bf2dd20
Norbert Považanec <povazanecn@gmail.com>
2026-03-19 01:08:59 +0000

    File changes

--- src/pages/MalfiHome.jsx
+++ src/pages/MalfiHome.jsx
@@ -30,6 +30,7 @@ export default function MalfiHome() {
       <OliveBranchDivider />
       <ReservationCTA />
       <GallerySection />
+      <AppDownloadSection />
       <LocationHours />
       <InstagramStrip />
     </div>
